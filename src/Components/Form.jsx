a5ffc2c31c4d9c4c76a38a43fe3a8951f5fab2b5
Eng Soke <cabdicasiismaxamed273@gmail.com>
2024-07-17 09:37:54 +0300

    waxaan ku daray form kasoo aan kuu ogolaaneenin inaad save gareso adigoon wax soo gelin

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,5 +1,30 @@
-
+import { useState } from "react"
 function Form ({handle}){
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [error, setError]= useState(false)
+
+
+
+    /// validate the form
+
+    const handleForm = (event) =>{
+        event.preventDefault()
+         if(title == "" && description == ""){
+            setError(true)
+         }
+         setTimeout(()=>{
+            setError(false)
+         },4000)
+    }
+
+
+
+
+
+
+
 
     return <div  className=" bg-second h-screen w-full absolute top-0">
 
@@ -15,14 +40,25 @@ function Form ({handle}){
             <div className="p-5  ">
 
             <i onClick={handle}  class="text-3xl absolute right-3 fa-solid fa-xmark"></i>
-        
+
+             {
+                
+                error == true ? <p className="text-red-500">Fadlan soo buuxi Form Ka</p> : ""
+             }
+
                <br/>
                <br/>
-                <input className="py-3 border-2 border-second  rounded sm:w-[300px] w-[260px] " type="text" placeholder="Enter Your Name"></input>
-                <textarea className="sm:w-[300px] w-[260px] rounded mt-3 border-2 border-second" rows={8} placeholder="Enter Title"/>
+                <input value={title} onChange={(event)=>{
+                    setTitle(event.target.value)
+                }} className="py-3 border-2 border-second  rounded sm:w-[300px] w-[260px] " type="text" placeholder="Enter Your Name"></input>
+               
+               
+                <textarea value={description} onChange={(event=>{
+                    setDescription(event.target.value)
+                })} className="sm:w-[300px] w-[260px] rounded mt-3 border-2 border-second" rows={8} placeholder="Enter Title"/>
                 
 
-                    <button className="bg-second px-20 py-2 m-10"> Save</button>
+                    <button onClick={handleForm} className="bg-second px-20 py-2 m-10"> Save</button>
             </div>
 
 
